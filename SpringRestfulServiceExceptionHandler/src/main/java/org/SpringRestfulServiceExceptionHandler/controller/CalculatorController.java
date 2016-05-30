ff6b485ebeb58a60a31e9a3033d84d64ceac4d83
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-05-30 23:02:47 +0100

    Calculator Controller

---
+++ SpringRestfulServiceExceptionHandler/src/main/java/org/SpringRestfulServiceExceptionHandler/controller/CalculatorController.java
@@ -0,0 +1,39 @@
+package org.SpringRestfulServiceExceptionHandler.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @RequestMapping("/sum")
+    public ResponseEntity<Object> sum(@RequestParam(value = "num1") String num1, @RequestParam(value = "num2") String num2) {
+
+        int sum = 0;
+
+        try {
+            int value1 = Integer.parseInt(num1);
+            int value2 = Integer.parseInt(num2);
+            sum = value1 + value2;
+        } catch (NumberFormatException ex) {
+            return new ResponseEntity<Object>("ERROR MESSAGE",HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Object>(sum, HttpStatus.OK);
+    }
+
+    @RequestMapping("/sumAdvice")
+    public ResponseEntity<Object> sumAdvice(@RequestParam(value = "num1") String num1, @RequestParam(value = "num2") String num2) throws NumberFormatException {
+        int sum = 0;
+
+        int value1 = Integer.parseInt(num1);
+        int value2 = Integer.parseInt(num2);
+        sum = value1 + value2;
+
+        return new ResponseEntity<Object>(sum, HttpStatus.OK);
+    }
+
+}
